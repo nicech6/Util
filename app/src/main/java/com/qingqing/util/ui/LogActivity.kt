b8526ac9b52f54a72d038afb368116cced88f02a
cuihai <nicech6@163.com>
2020-05-21 23:54:07 +0800

    commit

--- app/src/main/java/com/qingqing/util/ui/LogActivity.kt
+++ app/src/main/java/com/qingqing/util/ui/LogActivity.kt
@@ -1,6 +1,7 @@
 package com.qingqing.util.ui
 
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Toast
 import com.qingqing.util.BaseActivity
 import com.qingqing.util.R
@@ -13,26 +14,61 @@ import java.io.File
 
 class LogActivity : BaseActivity() {
 
-    var info: AppInfo? = null
+    lateinit var info: AppInfo
+    lateinit var dir: File
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
         if (intent != null) {
             info = intent.getParcelableExtra<AppInfo>(Extra.APPINFO)
-            if (false) {
-                sc_log.isChecked = true
-            } else {
-                sc_log.isChecked = info?.packageName?.let { exist(it) }!!
-            }
+            sc_log.isChecked = isExistExternalRootCacheFile(info.packageName)
         }
         sc_log.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (debug()) {
-                sc_log.isChecked = !isChecked
-                Toast.makeText(this, "DEBUG模式下已经开启LOG日志", Toast.LENGTH_LONG).show()
+            //            if (debug()) {
+//                sc_log.isChecked = !isChecked
+//                Toast.makeText(this, "DEBUG模式下已经开启LOG日志", Toast.LENGTH_LONG).show()
+//            } else {
+//
+//            }
+            if (isChecked) {
+                if (!isExistExternalRootCacheFile(info.packageName)) {
+                    if (isSDCardExists()) {
+                        var mkdir = dir.mkdirs()
+                        if (mkdir) {
+                            Toast.makeText(this, "已开启Log", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
             } else {
-
+                var delete = AppUtils.delete(dir)
+                if (delete) {
+                    Toast.makeText(this, "已关闭Log", Toast.LENGTH_LONG).show()
+                }
             }
         }
+    }
+
+
+    private fun isSDCardExists(): Boolean {
+        return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+    }
+
+    private fun isExistExternalRootCacheFile(packageName: String): Boolean {
+        return if (isSDCardExists()) {
+            dir = File(
+                Environment.getExternalStorageDirectory(),
+                "Android/data/" + packageName + "/cache"
+            )
+            dir.exists() && dir.isDirectory()
+        } else {
+            dir = File(
+                Environment.getDataDirectory(),
+                "Android/data/" + packageName + "/cache"
+            )
+            dir.exists() && dir.isDirectory()
+        }
+        return false
+
     }
 
     private fun debug(): Boolean {
--- app/src/main/java/com/qingqing/util/util/AppUtils.java
+++ app/src/main/java/com/qingqing/util/util/AppUtils.java
@@ -13,6 +13,7 @@ import android.util.Log;
 
 import com.qingqing.util.dto.AppInfo;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -84,6 +85,57 @@ public class AppUtils {
 
         }
         return false;
+    }
+
+    /**
+     * 删除一个文件或者目录
+     */
+    public static boolean delete(File fileOrDir) {
+        if (fileOrDir == null || !fileOrDir.exists())
+            return false;
+
+        if (fileOrDir.isFile()) {
+            return fileOrDir.delete();
+        } else if (fileOrDir.isDirectory()) {
+            return clearDir(fileOrDir) && fileOrDir.delete();
+        }
+
+        return false;
+    }
+
+
+    /**
+     * 清空一个指定的目录以及里面所有包含的文件，支持嵌套
+     */
+    public static boolean clearDir(File dir) {
+
+        if (dir == null || !dir.exists() || dir.isFile())
+            return false;
+
+        boolean ret = true;
+        if (dir.isDirectory()) {
+            File[] fileList = dir.listFiles();
+            if (fileList != null && fileList.length > 0) {
+                for (File f : fileList) {
+                    if (f.isFile()) {
+                        if (!f.delete())
+                            ret = false;
+                    } else if (f.isDirectory()) {
+                        String[] subFiles = f.list();
+                        if (subFiles != null && subFiles.length > 0) {
+                            if (!clearDir(f))
+                                ret = false;
+                            if (!f.delete())
+                                ret = false;
+                        } else {
+                            if (!f.delete())
+                                ret = false;
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
     }
 
 }
